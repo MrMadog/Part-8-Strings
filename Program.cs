1a1d80ba3cd1d30ce8a76c3c469619f5f555e13d
Maddox Macnamara <tpmac001maddog@gmail.com>
2023-03-21 10:17:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part_8___Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
