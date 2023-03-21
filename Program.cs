a4dbac616a5489bce630a3140fe267b7ac8ec3e7
Maddox Macnamara <tpmac001maddog@gmail.com>
2023-03-21 10:52:59 -0400

    done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name1 = "aldworth";
+            string name2 = "Aldworth";
+            if (name1.Equals(name2))
+                Console.WriteLine("Same");
+            else
+                Console.WriteLine("Different");
+
+
+            string firstName, lastName, username, email, studentNumberString;
+            int studentNumber;
+
+            Console.Write("Enter your first name:  ");
+            firstName = Console.ReadLine().ToLower();
+            Console.Write("Enter your last name:  ");
+            lastName = Console.ReadLine().ToLower();
+            Console.Write("Enter your student number:  ");
+            studentNumber = Convert.ToInt32(Console.ReadLine());
+
+            studentNumberString = studentNumber + "";
+
+            username = lastName.Substring(0, 3) + firstName.Substring(0, 3) + studentNumberString.Substring(6,3);
+            email = firstName.Substring(0, 1) + lastName + "@parkside.ca";
+
+            Console.WriteLine(username);
+            Console.WriteLine(email);
         }
     }
 }
